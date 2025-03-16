167a1a0a6e3d0b9a1a51ae99ca942c6cd1b5eb66
must-compute <admin@must-compute.com>
2025-03-16 12:13:22 -0400

    maelstrom lin-kv: reduce latency in normal test for now

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ MAELSTROM="./maelstrom/maelstrom"
 BINARY="./target/debug/maelstrom-tutorial"
 
 if [ "$1" = "lin-kv" ]; then
-  cargo build && $MAELSTROM test -w lin-kv --bin $BINARY --time-limit 15 --log-stderr --node-count 3 --concurrency 2n --rate 100 --latency 120
+  cargo build && $MAELSTROM test -w lin-kv --bin $BINARY --time-limit 20 --log-stderr --node-count 3 --concurrency 2n --rate 100 # --latency 120
 elif [ "$1" = "lin-kv-nemesis" ]; then
   cargo build && $MAELSTROM test -w lin-kv --bin $BINARY --time-limit 15 --log-stderr --node-count 3 --concurrency 4n --rate 30 --nemesis partition --nemesis-interval 4 --latency 120
 else
