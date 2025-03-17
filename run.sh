395498098e3b356f6659bbc4740b50eeacd5e46e
must-compute <admin@must-compute.com>
2025-03-17 11:57:33 -0400

    temporarily alter nemesis settings

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ MAELSTROM="./maelstrom/maelstrom"
 BINARY="./target/debug/maelstrom-tutorial"
 
 if [ "$1" = "lin-kv" ]; then
-  cargo build && $MAELSTROM test -w lin-kv --bin $BINARY --time-limit 20 --log-stderr --node-count 3 --concurrency 2n --rate 100 # --latency 120
+  cargo build && $MAELSTROM test -w lin-kv --bin $BINARY --time-limit 20 --log-stderr --node-count 3 --concurrency 2n --rate 1 # --latency 120
 elif [ "$1" = "lin-kv-nemesis" ]; then
-  cargo build && $MAELSTROM test -w lin-kv --bin $BINARY --time-limit 15 --log-stderr --node-count 3 --concurrency 4n --rate 100 --nemesis partition --nemesis-interval 4 # --latency 120
+  cargo build && $MAELSTROM test -w lin-kv --bin $BINARY --time-limit 15 --log-stderr --node-count 3 --concurrency 4n --rate 20 --nemesis partition --nemesis-interval 3 # --latency 120
 else
   echo "unknown command"
 fi
